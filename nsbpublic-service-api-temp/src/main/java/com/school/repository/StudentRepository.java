95d3c8ec8e1a9dcc27873ba9c20c06374f790a7e
jpsiet <jpsiet@users.noreply.github.com>
2025-03-26 18:02:18 -0400

    updated

--- nsbpublic-service-api-temp/src/main/java/com/school/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.school.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.school.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
